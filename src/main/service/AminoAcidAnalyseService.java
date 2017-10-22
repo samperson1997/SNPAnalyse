018660963b09a95999b7d0a5e88c27a53030b701
151250128 <151250128@smail.nju.edu.cn>
2017-10-22 23:50:25 +0800

    <refactor>:<add interface and class of DefectAnalyse>

---
+++ src/main/service/AminoAcidAnalyseService.java
@@ -0,0 +1,12 @@
+package main.service;
+
+import java.util.Map;
+
+public interface AminoAcidAnalyseService {
+
+    /**
+     *
+     * @return
+     */
+    Map<String, String> getAcidAnalysis();
+}
--- src/main/serviceImpl/DefectRecognition.java
+++ src/main/serviceImpl/DefectRecognition.java
@@ -12,28 +12,34 @@ public class DefectRecognition implements DefectRecognitionService{
 
     @Override
     public Map<String, String> checkSingleDefect() {
+
         return null;
     }
 
     @Override
     public Map<String, String> checkExtraDefect() {
+
         return null;
     }
 
     @Override
     public Map<String, String> checkMissDefect() {
+
         return null;
     }
 
     private int getMaxOfRange(int start, int end) {
+
         return 0;
     }
 
     private Map<Integer, Integer> getSortedMap() {
+
         return null;
     }
 
     private Map<String, String> getRes(){
+
         return null;
     }
 
